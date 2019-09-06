f4abf3a7ec4bcef5d5f769bee801bc34a73675b6
Dylan <Dylanrijnsburger40@gmail.com>
2019-09-06 17:32:24 +0200

    Pseudocode Alg 1 added
    Added the pseudocode from the article to src/ndfs/nndfs/NDFS.java

--- src/ndfs/NDFS.java
+++ src/ndfs/NDFS.java
@@ -13,4 +13,31 @@ public interface NDFS {
      * @return whether the graph contains an accepting cycle.
      */
     public boolean ndfs();
+
+    /**
+     *
+     * PSEUDOCODE ALGORITHM 1
+     *
+     *  proc nndfs(sI)
+     *      dfs_blue(sI)
+     *      report no cycle
+     *
+     *  proc dfs_red(s)
+     *      for all t in post(s) do
+     *          if t.color = cyan
+     *              report cycle & exit
+     *          else if t.color = blue
+     *              t.color = red
+     *              dfs_red(t)
+     *
+     *  proc dfs_blue(s)
+     *      s.color := cyan
+     *      for all t in post(s) do
+     *          dfs_blue(t)
+     *      if s in A
+     *          dfs_red(s)
+     *          s.color := red
+     *      else
+     *          s.color = blue
+     */
 }
