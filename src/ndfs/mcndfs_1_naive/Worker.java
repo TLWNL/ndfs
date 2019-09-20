1340ef7fb4fe0ad70d30f979ccc5aec41296eea8
Louk Onkenhout <loukonkenhout@hotmail.com>
2019-09-20 09:58:39 +0200

    Finished up last pseudocode > code
    Added the last couple lines of pseudocode as code, sorta implemented the StateCount, needs to be coordinated, not sure if it is properly right now, i'll do more testing tho

--- src/ndfs/mcndfs_1_naive/Worker.java
+++ src/ndfs/mcndfs_1_naive/Worker.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import graph.Graph;
 import graph.GraphFactory;
 import graph.State;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * This is a straightforward implementation of Figure 1 of
@@ -13,16 +14,18 @@ import graph.State;
  * paper"</a>.
  */
 public class Worker implements Runnable{
+    //This bad boi is for shared count
+    private static StateCount stateCount = new StateCount();
+    private final ReentrantLock lock = new ReentrantLock(true); 
 
     private final Graph graph;
-    private static final Colors global_colors = new Colors();
+    private static final Colors globalColors = new Colors();
     private final Colors colors = new Colors();
-    private static int stateCount = 0;
-    private boolean result = false;
+    //private static int stateCount = 0;
+    private static boolean result = false;
 
-    public static void main(String[] args) {
+    private static boolean isDone = false;
 
-    }
     private final int id;
 
     // Throwing an exception is a convenient way to cut off the search in case a
@@ -40,11 +43,8 @@ public class Worker implements Runnable{
      *             is thrown in case the file could not be read.
      */
     public Worker(File promelaFile, int workerId) throws FileNotFoundException {
-
         this.graph = GraphFactory.createGraph(promelaFile);
         this.id = workerId;
-        // Creates a local hashmap for the colors per worker thread
-//        this.thread_colors = new Colors();
     }
 
     //Add an extra parameter: int N indicating which direction a thread should take
@@ -64,22 +64,30 @@ public class Worker implements Runnable{
                 s.count--;
                 while(!s.count == 0);
             }
-            s.red[i] = true;
+            s.red = true;
             s.pink[i] = false;
         */
         colors.color(s, Color.PINK);
         for (State t : graph.post(s)) {
             if (colors.hasColor(t, Color.CYAN)) {
                 throw new CycleFoundException();
-            } else if (!colors.hasColor(t, Color.PINK) && !global_colors.hasColor(t, Color.RED)) {
-//                colors.color(t, Color.RED);
+            } else if (!colors.hasColor(t, Color.PINK) && !globalColors.hasColor(t, Color.RED)) {
                 dfsRed(t, workerId);
             }
         }
-
         if (s.isAccepting()) {
+            lock.lock();
+            try{
+                stateCount.decrement(s);
+            } finally{
+                lock.unlock();
+            }
 
+            //condition.await(), need to fix this bitch over here, this most likely where it gets stuck
+            while(stateCount.currentCount(s) != 0);
         }
+        globalColors.color(s, Color.RED);
+        colors.color(s, Color.WHITE);
     }
 
     //Add an extra parameter: int i indicating which direction a thread should take
@@ -89,7 +97,7 @@ public class Worker implements Runnable{
             Pseudo:
             s.color[i] = CYAN
             for all t in post_b_i() do:
-                if(t.color[i] == white)
+                if(t.color[i] == white && !red)
                     report cycle and exit;
             if(s.isAccepting()){
                 s.count++;
@@ -101,16 +109,21 @@ public class Worker implements Runnable{
         colors.color(s, Color.CYAN);
         for (State t : graph.post(s)) {
             // Should check the local hashmap for the thread for white and the global for red!
-            if (colors.hasColor(t, Color.WHITE) && !global_colors.hasColor(t, Color.RED)) {
+            // <- that aint precisely the same tho, they always just check for  == white, i think ours should just do the same
+            if (colors.hasColor(t, Color.WHITE) && !globalColors.hasColor(t, Color.RED)) {
                 dfsBlue(t, workerId);
             }
         }
         if (s.isAccepting()) {
-            stateCount++;
+            lock.lock();
+            try{
+                stateCount.increment(s);
+            } finally{
+                lock.unlock();
+            }
             dfsRed(s, workerId);
-        } else {
-            colors.color(s, Color.BLUE);
-        }
+        } 
+        colors.color(s, Color.BLUE);
     }
 
     //Add an extra parameter: int N indicating which direction a thread should take
@@ -130,20 +143,26 @@ public class Worker implements Runnable{
         try {
             //This just to see that they both executing, can unbox the sleep down there if ya wanna see fo sho
             //then you also gotta uncomment the extra catch
-            System.out.printf("Current thread: %d\n", this.id);
+            //System.out.printf("Current thread: %d\n", this.id);
             
             /*Thread.sleep(10000);*/
             
             nndfs(graph.getInitialState(), this.id); //Add parameter here: int N to indicate traversal of current thread
+            //System.out.printf("It does finish the program tho\n");
+            isDone = true;
         } catch (CycleFoundException e) {
             result = true;
+            isDone = true;
         }
         /*catch(InterruptedException e){
             //ignore
         }*/
     }
 
-    public boolean getResult() {
+    public static boolean getResult() {
+        System.out.printf("Now waiting for result..\n");
+        while(!isDone || result);
+        System.out.printf("Result: %s\n", result);
         return result;
     }
 }
